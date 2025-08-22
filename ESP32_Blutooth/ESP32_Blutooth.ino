2532c2960a91e0bdb8324210d38326d4bdcdb19b
SalmanAlFarizK <alfarizksalman@gmail.com>
2025-08-22 15:53:41 +0530

    ESP32 integrated with sensors and sensor datas are sent over the bluetooth network.

---
+++ ESP32_Blutooth/ESP32_Blutooth.ino
@@ -0,0 +1,168 @@
+#include <SPI.h>
+#include <SD.h>
+#include <string.h>
+#include "BluetoothSerial.h"
+
+
+
+/*************************************
+* Pin map.
+*************************************/
+#define SD_CS 5
+#define PIN_RED    25
+#define PIN_GREEN  26
+#define PIN_BLUE   27
+
+#define TRIGGER_PIN 4
+#define ECHO_PIN    17
+
+// ----------------- Constants ----------------
+#define SOUND_SPEED       0.034f    // cm/us
+#define CM_TO_INCH        0.393701f
+#define MAX_NEAREST_DISTANCE_THRESHOLD_CM  10
+
+// ----------------- Globals ------------------
+long duration_us;
+float distance_cm;
+float distance_in;
+unsigned long currentMillis;
+unsigned long previousMillis = 0;
+BluetoothSerial SerialBT;
+
+typedef struct {
+  uint32_t uiFlahSaveSize;
+  uint32_t uiObcTime;              // simple seconds counter
+} DistanceCrossInfo;
+
+DistanceCrossInfo gtDistancInfo;
+
+/******************** Ultrasonic ********************/
+int UltraSonicSensorReading(void) {
+  // Trigger pulse
+  digitalWrite(TRIGGER_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
+
+  // Echo read
+  duration_us = pulseIn(ECHO_PIN, HIGH, 30000UL); // timeout 30ms
+  if (duration_us == 0) {
+    return 9999; // out of range / timeout
+  }
+
+  distance_cm = duration_us * SOUND_SPEED / 2.0f;
+  distance_in = distance_cm * CM_TO_INCH;
+  return (int)distance_cm;
+}
+
+/******************** RGB LED ********************/
+void setColor(int R, int G, int B) {
+  analogWrite(PIN_RED,   R);
+  analogWrite(PIN_GREEN, G);
+  analogWrite(PIN_BLUE,  B);
+}
+
+void RgbLedInit(void) {
+  pinMode(PIN_RED,   OUTPUT);
+  pinMode(PIN_GREEN, OUTPUT);
+  pinMode(PIN_BLUE,  OUTPUT);
+  // start off
+  setColor(0, 0, 0);
+}
+
+
+/*************************************
+* Bluetooth Functions.
+*************************************/
+void InitBlueTooth(void)
+{
+  Serial.begin(115200);
+
+  if (!SerialBT.begin("ESP32_SFK")) 
+  {
+    Serial.println("An error occurred initializing Bluetooth");
+  } 
+  else 
+  {
+    Serial.println("Bluetooth started. Waiting for a connection...");
+  }
+
+  return;
+}
+
+void SendToBlueTooth(char* pucData)
+{
+  if (SerialBT.hasClient()) 
+  {
+    SerialBT.println(pucData);
+    Serial.println("Data is sent to bluetooth!");
+  } 
+  else 
+  {
+    Serial.println("No device connected.");
+  }
+
+  return;
+}
+
+
+/******************** Distance Alert FSM ********************/
+void DistanceAlertFsm(void) {
+  int dist = UltraSonicSensorReading();
+
+  // Build message buffer
+  uint8_t msg[150] = {0};
+
+  if (dist < MAX_NEAREST_DISTANCE_THRESHOLD_CM) {
+    setColor(255, 0, 0);  // RED
+
+    int msgLen = snprintf((char*)msg, sizeof(msg),
+                          "The Distance is %d cm, OBC time: %u s",
+                          dist, gtDistancInfo.uiObcTime);
+
+    // (Optional) Save to flash
+    SendToBlueTooth((char*)msg);
+
+    Serial.print("ALERT -> ");
+    Serial.println((char*)msg);
+  } 
+  else {
+    setColor(0, 255, 0);  // GREEN
+    // You may also send the normal distance periodically if you want:
+    // snprintf((char*)msg, sizeof(msg), "Distance OK: %d cm", dist);
+    // Blynk.virtualWrite(V1, (char*)msg);
+  }
+}
+
+/******************** Time Ticker (1s) ********************/
+void TimeTicker(void) {
+  currentMillis = millis();
+  if ((currentMillis - previousMillis) >= 1000UL) {
+    previousMillis = currentMillis;
+    gtDistancInfo.uiObcTime += 1;
+  }
+}
+
+/******************** Setup & Loop ********************/
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Setup function Entered.");
+
+
+  RgbLedInit();
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  InitBlueTooth();
+
+  Serial.println("Setup done.");
+}
+
+void loop() {
+
+  TimeTicker();
+  DistanceAlertFsm();
+
+  // Keep delays small so Blynk stays responsive
+  delay(100);
+}
